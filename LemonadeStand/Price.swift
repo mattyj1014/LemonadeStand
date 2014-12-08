95d3fa1dcbdc593e65d3a60e9f0257077b517e78
Matthew Jenks <matthewjenks@Matthews-MacBook-Pro-3.local>
2014-12-07 19:02:35 -0700

    final v

---
+++ LemonadeStand/Price.swift
@@ -0,0 +1,16 @@
+//
+//  Price.swift
+//  LemonadeStand
+//
+//  Created by Matthew Jenks on 9/19/14.
+//  Copyright (c) 2014 FiveWest. All rights reserved.
+//
+
+import Foundation
+
+struct Price {
+    
+    let lemon = 2
+    let iceCube = 1
+    
+}
---
+++ LemonadeStand/Supplies.swift
@@ -0,0 +1,25 @@
+//
+//  Supplies.swift
+//  LemonadeStand
+//
+//  Created by Matthew Jenks on 9/19/14.
+//  Copyright (c) 2014 FiveWest. All rights reserved.
+//
+
+import Foundation
+
+struct Supplies {
+    var money = 0
+    var lemons = 0
+    var iceCubes = 0
+    
+    
+    init(aMoney: Int, aLemons:Int, aIceCubes: Int) {
+        money = aMoney
+        lemons = aLemons
+        iceCubes = aIceCubes
+        
+    }
+    
+    
+}
